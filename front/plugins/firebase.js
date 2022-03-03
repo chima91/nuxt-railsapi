8789ffaa9e76ae8dfd869a1fe7f3a47d03d8195e
web-novice-tech <itb4769we@gmail.com>
2022-03-03 17:01:04 +0900

    create signup page with firebase.auth()

--- front/plugins/firebase.js
+++ front/plugins/firebase.js
@@ -1,14 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+const fbConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
 };
+firebase.initializeApp(fbConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export default firebase;
